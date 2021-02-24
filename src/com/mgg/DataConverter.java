4e674c6b4c5e5b08ff749158e4ecbf40d46fe42d
brustrianrivas <69874133+brustrianrivas@users.noreply.github.com>
2021-02-24 14:11:14 -0600

    Add files via upload

---
+++ src/com/mgg/DataConverter.java
@@ -0,0 +1,20 @@
+package com.mgg;
+
+/**
+ * Program reads in data from .csv file converts it to the appropriate
+ * object and outputs data to properly formatted XML/JSON file.
+ * @author Byron Rustrian
+ * @author Eric Schlautman
+ *
+ */
+public class DataConverter {
+	public final void loadData() {
+		
+	}
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
